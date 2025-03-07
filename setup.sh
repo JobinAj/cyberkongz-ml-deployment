a01d35f7aac9b207ba51ae778568faa2dfe75200
Jobin Aj <jobinaj6361@gmail.com>
2025-03-07 16:47:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ docker build -t jobinaj/cyberkongz-ml:latest .
 docker push jobinaj/cyberkongz-ml:latest
 
 # Deploy using Kubeflow
-kubectl apply -f manifests/component.yaml --validate=false
+kubectl apply -f manifests/component.yaml 
 
 # Upload Kubeflow pipeline (assuming 'kubeflow-pipeline.py' is a pipeline script)
 python manifests/kubeflow-pipeline.py
