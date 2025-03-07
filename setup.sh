ec4bf5832ed73fe97a1ac71497a1037efa4a42af
Jobin Aj <jobinaj6361@gmail.com>
2025-03-07 12:33:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build Docker Image
-docker build -t your-docker-repo/cyberkongz-ml:latest .
+docker build -t jobinaj/cyberkongz-ml:latest .
 
 # Push to Docker Hub
-docker push your-docker-repo/cyberkongz-ml:latest
+docker push jobinaj/cyberkongz-ml:latest
 
 # Deploy using Kubeflow
 kubectl apply -f manifests/component.yaml
